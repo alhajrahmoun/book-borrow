328d7fe0964c7a4314bf67d10a62144277deca72
mhdrahmoun <mrahmoun93@gmail.com>
2016-10-18 21:59:23 +0300

    ready to deploy

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,5 +1,5 @@
 class Api::BooksController < ApplicationController
-	before_action :authenticate_user!
+	#before_action :authenticate_user!
 	before_action :check_if_approved, only: [:subscribe]
 	
 	def show
